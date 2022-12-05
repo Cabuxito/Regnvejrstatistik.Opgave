187f3c852c64bbe00fa327b6132af2e44e3e4cae
Brian Gastón di Gangi Pedersen <briandigangi@hotmail.com>
2022-12-05 14:48:58 +0100

    +Base structure for program

--- Regnvejrstatistik.Opgave/Program.cs
+++ Regnvejrstatistik.Opgave/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿double[] regnVejVærdier = new double[5];
+int valg = 0;
+
+do
+{
+    Console.WriteLine("Regnvejrstatistik");
+    Console.Write("1- Indtast Data\n2- Vis alle værdier\n3- Gennemsnittet\n4- Max Værdi\n5- Min Værdi\nDit valg: ");
+	try
+	{
+		valg = Convert.ToInt32(Console.ReadLine());
+	}
+	catch (Exception)
+	{
+		Console.WriteLine("ERROR - Prøv igen..\nTryk noget for at forsæt.");
+		Console.ReadKey();
+		Console.Clear();
+		continue;
+	}
+	switch (valg)
+	{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		default:
+			break;
+	}
+
+} while (true);
